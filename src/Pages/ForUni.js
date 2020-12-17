30b6ea7ba216f2411cc5bae5ed6fc103f142aef4
PieroVe <45082124+PieroVioleta@users.noreply.github.com>
2020-12-17 03:55:38 -0500

    cambios menores

--- src/Pages/ForUni.js
+++ src/Pages/ForUni.js
@@ -2,31 +2,30 @@ import React, { Component } from 'react';
 import { Button, TextField, Grid } from '@material-ui/core';
 import Question from './ComponentsForUni/Question'
 class ForUni extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {info: ['', '', '', '', '']}
+    }
+    
+    enviarPregunta() {
+        var element = document.getElementById("entradaTexto")
+        var questionText = element.value
+        var elm = this.state.info
+        elm = [questionText].concat(elm)
+        this.setState({info:elm})
+    }
     render(){
-        function enviarPregunta() {
-            // var element = document.getElementById("entradaTexto")
-            // var questionText = element.value
-            // var preguntas = document.getElementById("seccion-preguntas")
-            // preguntas.innerHTML += "<Question pregunta={questionText}/>"
-        }
         return(
             <div>
                 <div id="contenedor-pregunta">
                     <form id="form_pregunta">
                         <span id="descripcionPregunta">Realiza una pregunta</span>
                         <input id="entradaTexto" type="text" name="name" size="100"/>
-                        <button id="boton" type="button" onClick={enviarPregunta}>Preguntar</button>
+                        <button id="boton" type="button" onClick={()=>this.enviarPregunta(  )}>Preguntar</button>
                     </form>
                 </div>
                 <div id="seccion-preguntas">
-                    <Question/>
-                    <Question/>
-                    <Question/>
-                    <Question/>
-                    <Question/>
-                    <Question/>
-                    <Question/>
-
+                    {this.state.info.map(elm => <Question pregunta={elm}/>) }
                 </div>
             </div>
             
